fb22e9f957d02dbe7109082136d3cbb433dff094
Gaurav Sethi <gauravsethi0404@gmail.com>
2020-05-21 15:09:21 -0400

    basic tutorial

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKPsCkXMedH4p9jbekpPVXIdkhLTNuh4o",
+  authDomain: "deliveryservice-9a25c.firebaseapp.com",
+  databaseURL: "https://deliveryservice-9a25c.firebaseio.com",
+  projectId: "deliveryservice-9a25c",
+  storageBucket: "deliveryservice-9a25c.appspot.com",
+  messagingSenderId: "38530961664",
+  appId: "1:38530961664:web:da502585f395f50c5563e5",
+  measurementId: "G-DP1CXC93Q7",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
